a8e8388c7e220ce926285d454ce3287e801ee87b
daffarez <daffaelguska@gmail.com>
2022-10-26 22:48:41 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <div class="nav-bar"></div>
+    <ProductsSocks
+      :product="product"
+      :image="image"
+      :inStock="false"
+      :details="productDetail"
+      :variants="variant"
+      :cart="cart"
+      :addToCart="addToCart"
+      :updateProduct="updateProduct"
+    />
+  </div>
+</template>
+
+<script>
+import ProductsSocks from "@/components/Products/Products.vue";
+
+export default {
+  name: "App",
+  components: {
+    ProductsSocks,
+  },
+  data() {
+    return {
+      product: "Socks",
+      image: "vmSocks-green.jpeg",
+      productDetail: ["80% cotton", "20% polyster", "Gender-neutral"],
+      variant: [
+        {
+          variantId: 1234,
+          variantColor: "green",
+          variantImage: "vmSocks-green.jpeg",
+        },
+        {
+          variantId: 1235,
+          variantColor: "blue",
+          variantImage: "vmSocks-blue.jpeg",
+        },
+      ],
+      cart: 0,
+    };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  display: flex;
+  flex-flow: wrap;
+  padding: 1rem;
+}
+.nav-bar {
+  background: linear-gradient(-90deg, #84cf6a, #16c0b0);
+  height: 60px;
+  margin-bottom: 15px;
+}
+</style>
---
+++ src/components/Products/Products.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="product">
+    <div class="product-image">
+      <img :src="require(`@/assets/${image}`)" />
+    </div>
+    <div class="product-information">
+      <h1>{{ product }}</h1>
+      <p v-if="inStock">In Stock</p>
+      <p v-else>Out of Stock</p>
+      <ul>
+        <li v-for="(detail, index) in details" :key="index">{{ detail }}</li>
+      </ul>
+      <div v-for="variant in variants" :key="variant.variantId">
+        <p @mouseover="updateProduct(variant.variantImage)">
+          {{ variant.variantColor }}
+        </p>
+      </div>
+      <button @click="addToCart">Add to Cart</button>
+      <div class="cart">
+        <p>Cart ({{ cart }})</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductsSocks",
+  props: {
+    product: String,
+    image: String,
+    inStock: Boolean,
+    details: Array,
+    variants: Array,
+    cart: Number,
+    addToCart: Function,
+    updateProduct: Function,
+  },
+};
+</script>
+
+<style>
+@import "./product_styles.css";
+</style>
